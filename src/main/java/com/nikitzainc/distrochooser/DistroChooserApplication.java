6025eea45d0e2d69895913d567cc929f36323f54
Никита Ермилов <ermilov.nv@edu.spbstu.ru>
2025-09-08 16:09:07 +0300

    Init

---
+++ src/main/java/com/nikitzainc/distrochooser/DistroChooserApplication.java
@@ -0,0 +1,13 @@
+package com.nikitzainc.distrochooser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistroChooserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistroChooserApplication.class, args);
+    }
+
+}
